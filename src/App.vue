9f4fc3b94452523d6bb59a3e7859bd668885aac3
Jared Collier <jared.j.collier@gmail.com>
2018-06-17 08:29:20 -0400

    create base webpack app
    vue-cli was used to generate app scaffolding.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.svg" class="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  max-width: 8em;
+}
+</style>
