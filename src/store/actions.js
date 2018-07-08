cb363f600d774fc4e86b468ae390c4c7242d6667
Jared Collier <jared.j.collier@gmail.com>
2018-07-08 08:57:58 -0400

    fixes linting errors

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,15 +2,13 @@ import db from '../database/db';
 
 export default {
   fetchAllClasses ({ state, commit }) {
-
     console.log( `🔥‍ fetching classes` );
 
     return new Promise(( resolve, reject ) => {
-
       db.collection( 'classes' ).onSnapshot( querySnapshot => {
         const classesArray = querySnapshot.docs[ 0 ].data().modules;
 
-        classesArray.forEach( ( newClass, index ) => {
+        classesArray.forEach(( newClass, index ) => {
           commit( 'setItem', { resource: 'classes', id: index, item: newClass });
         });
 
